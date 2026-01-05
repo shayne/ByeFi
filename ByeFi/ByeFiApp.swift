3555b3bc936e5330029cad1430b44c2006b138c3
shayne <79330+shayne@users.noreply.github.com>
2026-01-05 15:07:14 -0500

    Initial Commit

---
+++ ByeFi/ByeFiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ByeFiApp.swift
+//  ByeFi
+//
+//  Created by Shayne Sweeney on 1/5/26.
+//
+
+import SwiftUI
+
+@main
+struct ByeFiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
